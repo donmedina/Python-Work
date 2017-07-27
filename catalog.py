17942026c8ed692438b4d425aba59d935f29bca8
Gabriel Medina <medina.rj@live.com>
2017-07-27 00:58:07 -0300

    Adjustments

--- catalog.py
+++ catalog.py
@@ -19,7 +19,7 @@ contador = 0
 
 lc = pd.DataFrame()
 
-for contador in range(1, np):
+for contador in range(1, np + 1):
     page_u = requests.get("https://www.nuuvem.com/catalog/sort/title/sort-mode/asc/page/%s" %contador)
     soup = BeautifulSoup(page_u.content,'html.parser')
     catalogo = soup.find(class_="products-dock--main nvm-mod mod-group-sell mod-group-sell-offer")
@@ -28,11 +28,10 @@ for contador in range(1, np):
     valor_decimal = [vd.get_text() for vd in catalogo.select(".decimal")]
     
     l1 = pd.Series(titulo, name='titulo')
-    l2 = pd.Series(sem_desconto, name='sem_desconto')
     l3 = pd.Series(valor_inteiro, name='inteiro')
     l4 = pd.Series(valor_decimal, name='decimal')
     
-    lista = pd.concat ([l1,l2,l3,l4], axis=1)
+    lista = pd.concat ([l1,l3,l4], axis=1)
     print (lista)
     lc = lc.append(lista)
 
